815e27791188d2eed5c4570ff02c3bb627253808
Your Name <you@example.com>
2023-11-21 16:02:14 -0800

    Built the Constructer, started updateRender() function --J

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -1,7 +1,13 @@
 #include "ComplexPlane.h"
 
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight) {
-
+    m_pixel_size = Vector2i(pixelWidth, pixelHeight);
+    float m_aspectRatio = (pixelWidth*1.0)/(pixelHeight*1.0);
+    m_plane_center = Vector2f(0,0);
+    m_plane_size = Vector2f(BASE_WIDTH, BASE_HEIGHT*m_aspectRatio);
+    int m_zoomCount = 0;
+    m_state = State::CALCULATING;
+    m_vArray = VertexArray(Points, pixelWidth*pixelHeight);
 }
 
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const {
@@ -9,7 +15,19 @@ void ComplexPlane::draw(RenderTarget& target, RenderStates states) const {
 }
 
 void ComplexPlane::updateRender() {
-
+    if(m_State == State.Calculating)
+    {
+        for(unsigned int j = 0; j < m_pixel_size.x; j++)//These paramenters don't 
+                                                        //look right to me I think 
+                                                        //its the m_pixel_size.x same 
+                                                        //with the y
+        {
+            for(unsigned int i = 0; i < m_pixel_size.y; i++)
+            {
+                m_vArray.position = ;
+            }
+        }
+    }
 }
 
 void ComplexPlane::zoomIn() {
